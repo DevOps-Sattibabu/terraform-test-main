cc488ebe8a2b60dd7245bc42dec545d184054ef5
babusatti <33149941+babusatti@users.noreply.github.com>
2025-07-23 16:43:04 +0530

    Update main.tf vm2

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ resource "aws_instance" "vm1" {
   instance_type = var.AWS_INSTANCE_TYPE
   tags = var.INSTANCE_NAME
 }
+resource "aws_instance" "vm2" {
+  ami = var.AWS_INSTANCE_AMI
+  instance_type = var.AWS_INSTANCE_TYPE
+  tags = {
+  name = DEV
+  }
+}
