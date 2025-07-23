2d3e5b961f4d546ef9049edc0ea4cc815aabc37c
babusatti <33149941+babusatti@users.noreply.github.com>
2025-07-23 19:26:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,11 +6,11 @@ output "instance_ip" {
     value = aws_instance.vm1.public_ip
     description = "this is instance vm1 ip addr"
 }
-output "instance_id" {
+output "instance_ids" {
   value = aws_instance.vm2.id
   description = "this is instance vm2 id"
 }
-output "instance_ip" {
+output "instance_ips" {
     value = aws_instance.vm2.public_ip
     description = "this is instance vm2 ip addr"
 }
